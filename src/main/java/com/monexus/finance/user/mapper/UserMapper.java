6508bd5631b5d83cc5292e8155c6020a114af0b4
Breno <breno.joaosalvadorperez200@gmail.com>
2026-07-24 22:30:00 -0300

    feat: add UserMapper for RegisterRequest and UserResponse conversion

---
+++ src/main/java/com/monexus/finance/user/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package com.monexus.finance.user.mapper;
+
+import com.monexus.finance.user.dto.request.RegisterRequest;
+import com.monexus.finance.user.dto.response.UserResponse;
+import com.monexus.finance.user.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "profileImage", ignore = true)
+    @Mapping(target = "emailVerified", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updateAt", ignore = true)
+    User toEntity(RegisterRequest request);
+
+    UserResponse toResponse(User user);
+}
